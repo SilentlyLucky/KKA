cafd18c27b907b5a8f873d34c00180e760784f05
Baxfain <severinusfabian@gmail.com>
2025-12-16 14:47:53 +0700

    Update config.py

--- Script/config.py
+++ Script/config.py
@@ -11,13 +11,21 @@ GRASS = 1
 DIRT = 2
 STONE = 3
 BEDROCK = 4
+SAND = 5
+COAL = 6
+IRON = 7
+DIAMOND = 8
 
 # --- DATABASE BLOCK ---
 BLOCK_DATA = {
-    GRASS: {'name': 'Grass', 'texture': '../Assets/Texture/grass_block_side_1024x1024.png'},
-    DIRT:  {'name': 'Dirt', 'texture': '../Assets/Texture/dirt_1024x1024.png'},
-    STONE: {'name': 'Stone', 'texture': '../Assets/Texture/cobblestone_1024x1024.png'},
-    BEDROCK: {'name': 'Bedrock', 'texture': '../Assets/Texture/bedrock_1024x1024.png'},
+    GRASS: {'name': 'Grass', 'texture': '../Assets/Textures/grass.png'},
+    DIRT:  {'name': 'Dirt', 'texture': '../Assets/Textures/dirt.png'},
+    STONE: {'name': 'Stone', 'texture': '../Assets/Textures/cobblestone.png'},
+    BEDROCK: {'name': 'Bedrock', 'texture': '../Assets/Textures/bedrock.png'},
+    SAND: {'name': 'Sand', 'texture': '../Assets/Textures/sand.png'},
+    COAL: {'name': 'Coal', 'texture': '../Assets/Textures/coal_ore.png'},
+    IRON: {'name': 'Iron', 'texture': '../Assets/Textures/iron_ore.png'},
+    DIAMOND: {'name': 'Diamond', 'texture': '../Assets/Textures/diamond_ore.png'},
 }
 
 # --- BG ---
